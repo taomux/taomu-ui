1dfd72818c8076d14f3ec8735043d05f09b2b71e
lanten <lanten233@qq.com>
2024-06-09 18:45:45 +0800

    feat: Loading 方案替换

--- src/components/loading/loading.styled.ts
+++ src/components/loading/loading.styled.ts
@@ -3,7 +3,14 @@ import { css } from '@emotion/react'
 import { setGlobalCssVars, linkCssVar } from '../../styles'
 
 export interface LoadingCssVars {
+  /** 加载器颜色 */
   loadingColor?: string
+  /** 加载器尺寸 */
+  loadingSize?: number | string
+  /** 线条粗细 */
+  loadingWeight?: number | string
+  /** 旋转速度 s | ms */
+  loadingSpeed?: string
 }
 
 setGlobalCssVars('common', {
@@ -11,7 +18,26 @@ setGlobalCssVars('common', {
 })
 
 export const loadingStyled = css`
-  stroke: ${linkCssVar('loadingColor')};
+  border-radius: 50%;
+  cursor: progress;
+  display: inline-block;
+  overflow: hidden;
+  height: ${linkCssVar('loadingSize')};
+  width: ${linkCssVar('loadingSize')};
+  border-width: ${linkCssVar('loadingWeight')};
+  border-style: solid;
+  border-color: color-mix(in srgb, ${linkCssVar('loadingColor')}, transparent 75%);
+  border-top-color: ${linkCssVar('loadingColor')};
+  animation: animation-rotate ${linkCssVar('loadingSpeed')} linear infinite;
+
+  @keyframes animation-rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `
 
 declare global {
