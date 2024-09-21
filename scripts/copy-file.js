f9f4f8b66d93f6f37c04ef1fde79ac41e0532093
lanten <lanten233@qq.com>
2024-09-21 19:12:14 +0800

    doc: add LICENSE file

--- scripts/copy-file.js
+++ scripts/copy-file.js
@@ -6,3 +6,4 @@ const distPath = path.join(rootPath, './dist')
 
 fs.copyFileSync(path.join(rootPath, './package.json'), path.join(distPath, './package.json'))
 fs.copyFileSync(path.join(rootPath, './README.md'), path.join(distPath, './README.md'))
+fs.copyFileSync(path.join(rootPath, './LICENSE'), path.join(distPath, './LICENSE'))
