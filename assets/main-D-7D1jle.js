8d8a289c8ba525e6d9b6c35757adf907a49c1f30
lanten <lanten@users.noreply.github.com>
2025-03-01 18:18:17 +0000

    deploy: b7c5167db1c47e072683f5262c5034ba9fdcb6d5

--- assets/main-D-7D1jle.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-CLpGMVip.js";import{useMDXComponents as e}from"./index-npCMkmsx.js";import{ae as i}from"./index-Drb3RwmB.js";import"./index-CZMpeKRu.js";import"./iframe-Ret9SzVk.js";import"../sb-preview/runtime.js";import"./index-2nx3i45w.js";import"./index-D-TIQtLp.js";import"./index-Cu4lwwaE.js";import"./index-NOh9rqHv.js";import"./index-ogSvIofg.js";function r(n){const o={a:"a",h1:"h1",p:"p",...e(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"介绍"}),`
-`,t.jsx(o.h1,{id:"taomu-ui",children:"Taomu UI"}),`
-`,t.jsx(o.p,{children:"专为桌面端应用设计的 UI 库"}),`
-`,t.jsxs(o.p,{children:["更多类型文档: ",t.jsx(o.a,{href:"https://taomu-org.github.io/taomu-ui/type-doc/",rel:"nofollow",children:"Type Doc"})]}),`
-`,t.jsx(o.p,{children:"开发中..."})]})}function f(n={}){const{wrapper:o}={...e(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{f as default};
