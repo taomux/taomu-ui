2e0804759109fbade2cac6e4c408fa51ca914114
lanten <lanten@users.noreply.github.com>
2025-04-23 15:00:53 +0000

    deploy: 0ec86f65bb6ee8ed9e12c89c9d5f1830dd7ddb63

--- assets/form-item.stories-tzZSmAnV.js
+++
@@ -1,6 +0,0 @@
-import{a as m}from"./mini-scrollbar-1dk_y2Qh.js";import{F as s}from"./form-item-CIn-fHDr.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-D4lIrffr.js";import"./use-taomu-classname-Dqq3FQ7F.js";const i={title:"Components/Form/FormItem",component:s,tags:["autodocs"],argTypes:{}},r={args:{},render:()=>m("div",{children:"FormItem 需要配合 Form 使用，本实例仅用于文档展示"})};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {},
-  render: () => {
-    return <div>FormItem 需要配合 Form 使用，本实例仅用于文档展示</div>;
-  }
-}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const F=["基础示例"];export{F as __namedExportsOrder,i as default,r as 基础示例};
