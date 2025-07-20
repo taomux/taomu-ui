49a496674d31ffd087ea68af49735137f166a39e
lanten <lanten233@qq.com>
2025-07-20 23:15:26 +0800

    chore: upgrade Storybook dependencies to version 9.0.17 and update import paths
    - Updated Storybook dependencies in package.json to version 9.0.17.
    - Changed import statements in all component story files from '@storybook/react' to '@storybook/react-vite'.
    - Updated import statements in main.mdx and ssr.mdx to use '@storybook/addon-docs/blocks' instead of '@storybook/blocks'.

--- src/components/input/input.stories.tsx
+++ src/components/input/input.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import { IconActivity } from '../icons'
 import { Input, InputPassword, InputTextarea } from './'
--- src/components/menu/menu.stories.tsx
+++ src/components/menu/menu.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 
 import { IconActivity, IconAlertCircle, IconAlertTriangle, IconAlignLeft, IconSave } from '../icons'
 import { Button } from '../button'
